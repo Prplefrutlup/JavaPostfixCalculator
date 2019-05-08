4f9b4237d281b1c2327a02b207b13e046100f8fe
prplefrutlup <cb000016@gmail.com>
2019-05-07 20:37:22 -0700

    Initial commit

---
+++ src/com/prple/postfixcalc/Main.java
@@ -0,0 +1,16 @@
+package com.prple.postfixcalc;
+
+public class Main {
+
+    public static void main(String [] args) {
+
+        int data1 = 1;
+        int data2 = 2;
+        int data3 = 4;
+        int data4 = 5;
+        int data5 = 3;
+
+
+    }
+
+}
---
+++ src/com/prple/postfixcalc/PostfixCalculator.java
@@ -0,0 +1,13 @@
+package com.prple.postfixcalc;
+
+import java.util.Stack;
+
+public class PostfixCalculator extends Stack {
+
+    public PostfixCalculator() {
+    }
+
+
+
+
+}

f0ab582b3694f0c4c2c1df4727686f17a6935ef4
prplefrutlup <cb000016@gmail.com>
2019-05-11 10:59:20 -0700

    Linkedstack postfix calc

--- src/com/prple/postfixcalc/Postfix.java
+++ src/com/prple/postfixcalc/Postfix.java
@@ -2,12 +2,8 @@ package com.prple.postfixcalc;
 
 import java.util.Scanner;
 
-enum data {
-
-    data1, data2, data3, data4, data5;
-}
-
 public class Postfix {
+
 
     public static int evaluatePostfix(String postfix) {
 
@@ -18,16 +14,39 @@ public class Postfix {
         //scans the characters in the expression one by one
         while (sc.hasNext()) {
 
-            //if statement to check if character is an operand or number,
+            //if statement to check if character is an operand or string,
             //if a number - pushed to stack
-            if (sc.hasNextInt()) {
-                calculatorStack.push(sc.nextInt());
+            if (sc.hasNext()) {
+
+                String stringToInt = sc.next();
+
+                switch (stringToInt) {
+                    case "data1":
+                        int data1 = 1;
+                        calculatorStack.push(data1);
+                        break;
+                    case "data2":
+                        int data2 = 2;
+                        calculatorStack.push(data2);
+                        break;
+                    case "data3":
+                        int data3 = 4;
+                        calculatorStack.push(data3);
+                        break;
+                    case "data4":
+                        int data4 = 5;
+                        calculatorStack.push(data4);
+                        break;
+                    case "data5":
+                        int data5 = 3;
+                        calculatorStack.push(data5);
+                        break;
+                }
             }
 
-                //if characters scanned are operators,
-                // pop 2 elements and apply operator
+            //if characters scanned are operators,
+            // pop 2 elements and apply operator
             else {
-
 
                 int operandOne = calculatorStack.pop();
                 int operandTwo = calculatorStack.pop();
@@ -59,24 +78,18 @@ public class Postfix {
 
     }
 
-
     public static void main(String[] args) {
 
-        int data1 = 1;
-        int data2 = 2;
-        int data3 = 4;
-        int data4 = 5;
-        int data5 = 3;
 
-        String postfix1 = data1 + " " + data2 + "+" + data3 + "*" + data4 + "-";
+        String postfix1 = "data1 data2 + data3 * data4 -";
 
-        System.out.println(postfix1);
 
-        //String postfix2 = "data1 data2 * data3 * data1 -/ data4 data5 +";
+        String postfix2 = "data1 data2 * data3 * data1 -/ data4 data5 +";
 
         System.out.println(evaluatePostfix(postfix1));
 
-        //System.out.println(evaluatePostfix(postfix2));
+        System.out.println(evaluatePostfix(postfix2));
+
 
 
     }
